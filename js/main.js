a46374a5723fb4a533a004037d15d0a62e94b0cb
Santiago Bestoso <santiaguitobestoso@gmail.com>
2022-08-11 19:19:41 -0300

    Preentrega 2 Proyecto Final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+// Inicializamos el array de carrito, luego lo utilizaremos para el LocalStorage:
+
+let carrito = [];
+
 const obtainProducts = localStorage.getItem('Listado de Productos');
 
-let parseProducts = JSON.parse(obtainProducts);
+//Parseo para obtener los productos y poder realizar el maquetado mediante DOM:
+
+const parseProducts = JSON.parse(obtainProducts);
 console.log(parseProducts);
 
 const sectionProducts = document.querySelector('#products');
@@ -13,9 +19,60 @@ parseProducts.forEach(product =>{
                                 <img src='${product.foto}' alt='' class='productPic'>
                                 <p class='price'>Precio: $${product.precio}</p>
                                 <p class='platform'>Plataforma: <span class='acento'>${product.plataforma}</span></p>
-                                <form action='#'>
-                                <button class='addProduct'> Agregar Producto </button>
-                                </form>`;
+                                <button class='addProduct' id='${product.id}'> Agregar Producto </button>
+                                `;
     sectionProducts.appendChild(nuevoProducto);
+
+    document.getElementById(`${product.id}`).onclick =()=> agregarCarrito(`${product.id}`);
 });
 
+
+// Creacion y seleccion de elementos:
+
+const sectionCarrito = document.getElementById('carrito');
+
+const totalCompra = document.createElement('div');
+const montoTotalCompra = document.createElement('h3');
+const cantProductos = document.createElement('p');
+const buttonEnd = document.createElement('button');
+
+totalCompra.className = 'contenedorCarrito';
+totalCompra.innerHTML = `<h2> Carrito </h2> `;
+montoTotalCompra.innerHTML = 'Monto: $0';
+cantProductos.innerHTML = 'Cantidad de productos: 0';
+buttonEnd.innerHTML = 'Finalizar Compra';
+buttonEnd.className = 'btnEnd';
+
+sectionCarrito.appendChild(totalCompra);
+totalCompra.appendChild(montoTotalCompra);
+totalCompra.appendChild(cantProductos);
+totalCompra.appendChild(buttonEnd);
+
+buttonEnd.onclick = () => {
+    const precioFinal = montoTotalCompra.innerText;
+    alert(`Fin del ${precioFinal}`);
+    vaciarCarrito();
+}
+
+function agregarCarrito(id){
+    carrito.push(parseProducts.find(prods => prods.id == id));
+    localStorage.setItem("Carrito", JSON.stringify(carrito));
+    console.log(carrito);
+    calcularCarrito();
+}
+
+function calcularCarrito(){
+    let total = 0;
+    for (const producto of carrito) {
+        total += producto.precio;
+    }
+    montoTotalCompra.innerText = 'Monto: $' + total;
+    cantProductos.innerText = 'Cantidad de Productos: ' + carrito.length;
+}
+
+function vaciarCarrito(){
+    montoTotalCompra.innerText = "0";
+    cantProductos.innerText = "0";
+    localStorage.removeItem('Carrito');
+    carrito = [];
+}
