6ae047d09147c6eeddc4b625fec591d2f5568b73
Santiago Bestoso <santiaguitobestoso@gmail.com>
2022-08-16 20:04:44 -0300

    operadores avanzados

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ const sectionProducts = document.querySelector('#products');
 
 parseProducts.forEach(product =>{
     let nuevoProducto = document.createElement('div');
+
     nuevoProducto.className = 'product';
     nuevoProducto.innerHTML = `
                                 <h3 class='title'> ${product.titulo} </h3>
@@ -25,8 +26,41 @@ parseProducts.forEach(product =>{
                                 `;
     sectionProducts.appendChild(nuevoProducto);
 
-    document.getElementById(`${product.id}`).onclick =()=> agregarCarrito(`${product.id}`);
+    $(`#${product.id}`).click((e)=>{
+        agregarCarrito(`${product.id}`)
+    })
 });
+
+// seleccion de links del menu para efectos:
+
+const links = $('a');
+
+links.click((e)=>{
+    if(links[0] == e.target){
+        links[0].style.color = '#8C1858';
+        links[1].style.color = '#f5f5f5e5';
+        links[2].style.color = '#f5f5f5e5';
+        links[3].style.color = '#f5f5f5e5';
+    }
+    else if(links[1] == e.target){
+        links[0].style.color = '#f5f5f5e5';
+        links[1].style.color = '#8C1858';
+        links[2].style.color = '#f5f5f5e5';
+        links[3].style.color = '#f5f5f5e5';
+    }
+    else if(links[2] == e.target){
+        links[0].style.color = '#f5f5f5e5';
+        links[1].style.color = '#f5f5f5e5';
+        links[2].style.color = '#8C1858';
+        links[3].style.color = '#f5f5f5e5';
+    }
+    else if(links[3] == e.target){
+        links[0].style.color = '#f5f5f5e5';
+        links[1].style.color = '#f5f5f5e5';
+        links[2].style.color = '#f5f5f5e5';
+        links[3].style.color = '#8C1858';
+    }
+})
 
 
 // Creacion y seleccion de elementos:
@@ -40,7 +74,7 @@ const buttonEnd = document.createElement('button');
 
 totalCompra.className = 'contenedorCarrito';
 totalCompra.innerHTML = `<h2> Carrito </h2> `;
-montoTotalCompra.innerHTML = 'Monto: $0';
+montoTotalCompra.innerHTML = '$0';
 cantProductos.innerHTML = 'Cantidad de productos: 0';
 buttonEnd.innerHTML = 'Finalizar Compra';
 buttonEnd.className = 'btnEnd';
@@ -52,8 +86,27 @@ totalCompra.appendChild(buttonEnd);
 
 buttonEnd.onclick = () => {
     const precioFinal = montoTotalCompra.innerText;
-    alert(`Fin del ${precioFinal}`);
-    vaciarCarrito();
+
+    // alert(`Fin del ${precioFinal}`);
+    Swal.fire({
+        title: 'Quieres realizar la compra?',
+        html: `<h4>Precio final: ${precioFinal}</h4>
+        <p> Agregamos un 10% de descuento por compras mayores a $10000 </p>`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Finalizar Compra'
+    }).then((result) => {
+        if (result.isConfirmed) {
+        Swal.fire(
+            'Compra Finalizada!',
+            'Tu compra se finalizo con exito.',
+            'success'
+        )
+        vaciarCarrito();
+        }
+    })
 }
 
 function agregarCarrito(id){
@@ -61,14 +114,22 @@ function agregarCarrito(id){
     localStorage.setItem("Carrito", JSON.stringify(carrito));
     console.log(carrito);
     calcularCarrito();
+    Swal.fire({
+        text: '¡Producto agregado al carrito!'
+    })
 }
 
 function calcularCarrito(){
     let total = 0;
     for (const producto of carrito) {
         total += producto.precio;
+
+        // para el desafio complementario:
+        let totalDescuento = total * 0.15;
+        let totalConDescuento = total - totalDescuento;
+        (total > 10000) ? console.log(totalConDescuento):console.log('no tiene descuento');
     }
-    montoTotalCompra.innerText = 'Monto: $' + total;
+    montoTotalCompra.innerText = total;
     cantProductos.innerText = 'Cantidad de Productos: ' + carrito.length;
 }
 
@@ -80,4 +141,5 @@ function vaciarCarrito(){
 }
 });
 
-// Para el desafio complementario de optimizacion agregaremos descuentos a ciertos productos con el operador ternario y a esos productosles pondremos un indicador de que tienen tanto porcentaje de descuento.
+// Para el desafio complementario de optimizacion agregaremos descuentos a ciertos productos con el operador ternario y a esos productos les pondremos un indicador de que tienen tanto porcentaje de descuento.
+
